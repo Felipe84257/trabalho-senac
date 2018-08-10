5ca3a91462db7eb1d339189a42db5e8606a62d79
Felipe84257 <41974922+Felipe84257@users.noreply.github.com>
2018-08-09 21:18:56 -0300

    pagcategoriagame 1.0

---
+++ pagCategoriaGames.php
@@ -0,0 +1,25 @@
+<?php
+include ('inc/incCabecalho.php');
+header ('Content-type: text/html; charset=UTF-8', true);
+?>
+<img src="imagem/gBox1.jpg" width= 30% height= 30%/><br/>
+<p>informações do produto
+The Witcher 3: Wild Hunt é um jogo de Rpg em um mundo amplo e dinâmico, não linear e de fantasia sombria baseado em uma história conduzida pelo próprio personagem, pelas escolhas do jogador e com combates estratégicos. O terceiro capítulo desta saga premiada aprimora todos os aspectos da série, com um sistema de combate mais fluido, novos Witcher Senses e Monster Hunting, alquimia aperfeiçoada, sinais mágicos, sistemas de habilidades e muitas outras inovações.Jogue no papel de um caçador de monstros por recompensa extremamente treinadoTreinados desde a infância, os bruxos sofrem mutações para ganhar habilidades, força e reflexos super-humanos para trazer equilíbrio ao mundo em que vivem, o qual está infestado de monstros. Destrua seus inimigos, atuando como um caçador profissional de monstros, armado com várias armas, poções mutagênicas e magias de combate que podem ser aprimoradas.Cace uma grande variedade de monstros exóticosDesde feras selvagens que espreitam os vales, até predadores sobrenaturais astutos que se escondem nas sombras das cidades densamente populosas.Invista suas recompensas no aprimoramento de seu arsenal e na compra de armaduras personalizadas, ou gaste com corridas de cavalos, jogos de cartas, lutas e outros prazeres que a noite oferece.Explore um mundo aberto de fantasia, onde cada ação muda o curso da sua históriaProjetado para aventuras sem fim, o gigantesco mundo aberto de The Witcher estabelece novos padrões em termos de tamanho, profundidade e complexidade.Faça escolhas que vão além do bem e do mal, depois encare suas consequências profundasViaje em um mundo aberto fantástico: explore ruínas esquecidas, cavernas e naufrágios, negocie com mercadores e ferreiros anões em cidades e cace nas planícies abertas, montanhas e mares. Negocie com generais traiçoeiros, bruxas desonestas e membros da corte corruptos para oferecer serviços sombrios e perigosos.
+1 Jogador</P>
+<br/><br/>
+<img src="imagem/gBox4.jpg" width= 30% height= 30%/><br/><br/><br/>
+<p>Global Offensive foi lançado inicialmente com quatro modos de jogo: Casual Clássico, competitivo, demolição e corrida armamentista, e vários mapas oficiais para cada um dos modos do jogo. O modo de jogo Deathmatch, e outros mapas oficiais, foram adicionados pós-libertação. A mecânica para o cenário Hostage (de reféns) foram reformadas pós-libertação. Novas armas, como a CZ-75 Auto,R8 REVOLVER foram adicionados pós lançamento. As estatísticas para as armas e o layout dos mapas foram modificados pós-lançamento para o equilíbrio competitivo.<p/>
+<br/><br/>
+<img src="imagem/gBox2.jpg" width= 30% height= 30%/><br/><br/><br/>
+<p>O jogo de corrida mais autêntico, bonito, intenso e tecnicamente avançado no planeta, abrangendo diversas categorias de automobilismo – desde kart a protótipos LM e todo o resto que há entre eles.
+<p/><br/><br/>
+<img src="imagem/gBox3.jpg" width= 30% height= 30%/><br/><br/><br/>
+<p>é um jogo intenso de ação e horror, aonde os jogadores irão até a Europa continental. Lá uma experiência falha da Horzine Biotech causou um surto de um vírus que se espalhou sem controle. Os espécimes estão em todo lugar e a civilização está devastada. As comunicações falham, os governos entraram em colapso e as forças militares foram erradicadas. Os sobreviventes da Europa se esconderam, mas precisam continuar lutando pelas suas vidas.
+<p/>
+<br/><br/>
+<img src="imagem/gBox5.jpg" width= 30% height= 30%/><br/><br/><br/>
+<p>O Garry’s Mod, disponível para Windows e Mac, é um game que se destaca por proporcionar a sensação de liberdade para o jogador fazer o que bem entender, utilizando modelos próprios do jogo ou até de outros da mesma companhia, a Volvo. Ele é pago e seu custo é de R$16,99 via Steam. Imagine um mundo onde você poderá fazer o que quiser, desde criar a sua casa com rodinhas até uma banheira móvel e encontre a materialização de seus desejos no Garry´s Mod.<p/>
+<img src="imagem/gBox6.jpg" width= 30% height= 30%/><br/><br/><br/>
+<p>Acumule dinheiro sendo o maior criminoso da cidade em GTA 5, inicie personalizando as roupas do seu personagem depois comece a onda de crimes que vão encher seus bolsos, roube carros, realize assaltos e assassinatos para lucrar e ficar famoso entre as gangues. Proteja-se pois todos vão querer sua cabeça!<p/>
+<?php
+include ('/inc/incRodape.php');
