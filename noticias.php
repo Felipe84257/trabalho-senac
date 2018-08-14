95c408ef8cd1273f74eb0475d192305fcaf6119e
eduardohenrique10 <du_du_0-10@hotmail.com>
2018-08-14 19:29:23 -0300

    noticias 1.0

---
+++ noticias.php
@@ -0,0 +1,32 @@
+
+<?php
+include ('inc/incCabecalho.php');
+?>
+<br><br><br><br><br><br><br><br>
+<div id="borda" >
+<h1 style=" color: red; font-size: 35px";> <i> Ubisoft interrompe desenvolvimento de Steep para o Switch </i> </h1>
+&nbsp&nbsp&nbsp&nbsp&nbsp<img id ="foto" src="imagem/1.jpg";>
+<p style="color: blue; font-size: 20px";>&nbsp&nbsp&nbsp&nbsp <b><i> Port do jogo foi anunciado para o console da Nintendo no ano passado </i></b> </p>
+
+</div>
+
+
+
+<div id= "borda" >
+<h1 style=" color: red; font-size: 35px";> <i> Série Torchlight tem novo RPG de ação anunciado </i> </h1>
+&nbsp&nbsp&nbsp&nbsp&nbsp<img id ="foto" src="imagem/2.jpg";>
+<p style="color: blue; font-size: 20px";>&nbsp&nbsp&nbsp&nbsp <b><i> Torchlight Frontiers tem lançamento previsto para o PlayStation 4, Xbox One e PC</i></b> </p>
+
+</div>
+
+
+<?php
+include ('inc/incRodape.php');
+?>
+<style>
+#foto { height: 155px; width: 200px; }
+#borda { border-style: solid; border-color: white; background: black; } 
+</style>
+
+
+
